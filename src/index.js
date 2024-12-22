4e5d5dafe3c3e145b7a89b9d6c354a258c0e58d0
chrwiencke <81775007+chrwiencke@users.noreply.github.com>
2024-12-22 11:55:08 +0100

    Add Hono framework for handling redirects and form submissions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,61 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono'
+const app = new Hono()
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+app.get('/:id', async (c) => {
+	try {
+		const id = c.req.param('id')
+		const linkFromKV = await c.env.KV.get(id);
+
+		if (!linkFromKV) {
+			return c.text("Sorry this link does not exist")
+		} else {
+			return Response.redirect(linkFromKV, 307);
+		}
+	} catch (error) {
+		console.error('Error handling redirect:', error);
+		return c.text('An error occurred', 500);
+	}
+})
+
+app.post('/:id', async (c) => {
+	try {
+		const id = c.req.param('id')
+
+
+		const idAlreadyExists = await c.env.KV.get(id)
+
+		if (idAlreadyExists) {
+			return c.text(id + ' Already exists')
+		}
+
+		const linkFromKV = await c.env.KV.put(id);
+
+	} catch (error) {
+		console.error('Error handling redirect:', error);
+		return c.text('An error occurred', 500);
+	}
+})
+
+app.get('/', (c) => {
+    return c.html(
+`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Form Submission with htmx</title>
+    <script src="https://unpkg.com/htmx.org@1.9.2"></script>
+</head>
+<body>
+    <form hx-post="/" hx-include="[name='userInput']" hx-params="*">
+        <label for="userInput">Enter your input:</label>
+        <input type="text" id="userInput" name="userInput" required>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
+`
+)
+})
+
+export default app
